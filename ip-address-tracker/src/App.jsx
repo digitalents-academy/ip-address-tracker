115a3cdd81f39e73ad4c4a7279d4b43972c2827a
Abdiqani Dable <dablabd@digitalentshelsinki.internal>
2024-01-11 10:07:15 +0200

    input title made

--- ip-address-tracker/src/App.jsx
+++ ip-address-tracker/src/App.jsx
@@ -1,12 +1,15 @@
-
+import React from "react"
+import "./styles/App.css"
 
 function App() {
-  
 
   return (
-    <>
-      
-    </>
+    <div className="main">
+      <div className="input-main">
+        <div className="input-title">IP Address Tracker</div>
+      </div>
+      <div className="map-main"></div>
+    </div>
   )
 }
 
