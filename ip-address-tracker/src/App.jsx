24d4f146289016b90bc064eaaf526630b287926a
Roope Kataja <kataja34@protonmail.com>
2024-01-15 11:46:59 +0200

    button fixed

--- ip-address-tracker/src/App.jsx
+++ ip-address-tracker/src/App.jsx
@@ -1,16 +1,29 @@
-import React from "react"
-import arrow from "./assets/images/icon-arrow.svg"
-import "./styles/App.css"
+import React from 'react';
+import axios from 'axios';
+import arrow from "./assets/images/icon-arrow.svg";
+import "./styles/App.css";
 
 function App() {
+
+  async function address() {
+    const apiURL = "https://geo.ipify.org/api/v2/country,city?apiKey=at_xqIYtoLkrk8OBkudtIizO5uRzKTVG&ipAddress=8.8.8.8";
+    const apiKey = "at_xqIYtoLkrk8OBkudtIizO5uRzKTVG";
+  
+    try {
+      const response = await axios.get(apiURL, { params: { apiKey: apiKey } });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Something went wrong getting Geolocation from API!', error);
+    }
+  }
 
   return (
     <div className="main">
       <div className="input-main">
         <div className="input-title">IP Address Tracker</div>
         <div className="input-field">
-        <input className="input"/>
-        <img src={arrow} className="input-arrow"/>
+        <input className="input" placeholder="Search for any IP address or domain"/>
+        <img src={arrow} className="input-arrow" onClick={() => address()}/>
       </div>
       </div>
       <div className="map-main"></div>
@@ -18,4 +31,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
