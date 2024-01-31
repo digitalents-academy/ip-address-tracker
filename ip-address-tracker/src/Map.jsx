1d548ffdd01837783615bae64e2094974339c06b
Abdiqani Dable <dablabd@digitalentshelsinki.internal>
2024-01-31 10:25:28 +0200

    babymama

--- ip-address-tracker/src/Map.jsx
+++ ip-address-tracker/src/Map.jsx
@@ -1,10 +1,17 @@
-import * as ReactLeaflet from "react-leaflet"
+import * as ReactLeaflet from "react-leaflet";
+import L from "leaflet";
+import redmarker from "./assets/images/map-pin.svg";
 import "leaflet/dist/leaflet.css";
 import styles from "./map.module.css"
 
 //handles map
 //add jsx to handle coordinates
 function Map({ coordinates, marker }) {
+
+  let redmarkerIcon = L.icon({
+    iconUrl: redmarker,
+    iconSize: [40, 65]
+  });
 
   return ( 
     <ReactLeaflet.MapContainer
@@ -16,7 +23,7 @@ function Map({ coordinates, marker }) {
             attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <ReactLeaflet.Marker key={marker[0]} position={marker}></ReactLeaflet.Marker>
+        <ReactLeaflet.Marker key={marker[0]} position={marker} icon={redmarkerIcon}></ReactLeaflet.Marker>
     </ReactLeaflet.MapContainer>
    );
 }
